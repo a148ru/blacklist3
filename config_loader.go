2501e4f46ee801206f49dbb2379752b7004ab730
a148ru <ant.band@mail.ru>
2026-01-19 10:30:22 +0300

    correct flags and env

--- config_loader.go
+++ config_loader.go
@@ -8,14 +8,21 @@ import (
 
 func loadConfigFromAllSources() *Config {
 	configPath := getEnvOrFlag("CONFIG_PATH", "config", "config.yaml")
-
 	cfg := loadConfig(configPath)
 
-	overrideBool(&cfg.Daemon.Enabled, "DAEMON_ENABLED", "daemon")
-	overrideInt(&cfg.Daemon.IntervalSeconds, "DAEMON_INTERVAL", "interval")
-	overrideString(&cfg.OutputDir, "OUTPUT_DIR", "output")
-	overrideString(&cfg.MD5File, "MD5_FILE", "md5")
-	overrideInt(&cfg.HTTP.TimeoutSeconds, "HTTP_TIMEOUT", "http-timeout")
+	flagInterval := flag.Int("interval", 0, "set the file update check interval")
+	flagOutput := flag.String("output","","specify the directory to write files")
+	flagMD5File := flag.String("md5","","specify a file to store checksums")
+	flagHttpTimeOut := flag.Int("http-timeout",0,"specify http timeout")
+	flagDaemon := flag.Bool("daemon", false,"specify a flag to run as a daemon")
+	flag.Parse()
+
+
+	overrideInt(&cfg.Daemon.IntervalSeconds, "DAEMON_INTERVAL", "interval", flagInterval)
+	overrideString(&cfg.OutputDir, "OUTPUT_DIR", "output", flagOutput)
+	overrideString(&cfg.MD5File, "MD5_FILE", "md5", flagMD5File)
+	overrideInt(&cfg.HTTP.TimeoutSeconds, "HTTP_TIMEOUT", "http-timeout", flagHttpTimeOut)
+	overrideBool(&cfg.Daemon.Enabled, "DAEMON_ENABLED", "daemon", flagDaemon)
 
 	return cfg
 }
@@ -28,9 +35,8 @@ func getEnvOrFlag(env, flagName, def string) string {
 	return def
 }
 
-func overrideString(target *string, env, flagName string) {
-	flagValue := flag.String(flagName, "", "")
-	flag.Parse()
+func overrideString(target *string, env, flagName string, flagValue *string) {
+
 
 	if *flagValue != "" {
 		*target = *flagValue
@@ -42,12 +48,12 @@ func overrideString(target *string, env, flagName string) {
 	}
 }
 
-func overrideBool(target *bool, env, flagName string) {
-	flagValue := flag.Bool(flagName, *target, "")
-	flag.Parse()
+func overrideBool(target *bool, env, flagName string, flagValue *bool) {
 
-	*target = *flagValue
-
+	if *flagValue {
+		*target = *flagValue
+		return
+	}
 	if v := os.Getenv(env); v != "" {
 		if b, err := strconv.ParseBool(v); err == nil {
 			*target = b
@@ -55,11 +61,12 @@ func overrideBool(target *bool, env, flagName string) {
 	}
 }
 
-func overrideInt(target *int, env, flagName string) {
-	flagValue := flag.Int(flagName, *target, "")
-	flag.Parse()
+func overrideInt(target *int, env, flagName string, flagValue *int) {
 
-	*target = *flagValue
+	if *flagValue != 0{
+		*target = *flagValue
+		return
+	}
 
 	if v := os.Getenv(env); v != "" {
 		if i, err := strconv.Atoi(v); err == nil {
