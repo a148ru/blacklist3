3c9d964fb69f90fcf0b822dd05cdc1ea7816f016
a148ru <ant.band@mail.ru>
2026-01-20 12:53:52 +0300

    add use ip

--- processor.go
+++ processor.go
@@ -8,8 +8,10 @@ import (
 	"strings"
 )
 
-var netRegex = regexp.MustCompile(`\b\d{1,3}(\.\d{1,3}){3}/\d{1,2}\b`)
+//var netRegex = regexp.MustCompile(`\b\d{1,3}(\.\d{1,3}){3}/\d{1,2}\b`)
+// \b(?:(?:25[0-5]|2[0-4]\d|1?\d?\d)(?:\.(?:25[0-5]|2[0-4]\d|1?\d?\d)){3})(?:/(?:3[0-2]|[12]?\d))?\b
 
+var netRegex = regexp.MustCompile(`\b(?:(?:25[0-5]|2[0-4]\d|1?\d?\d)(?:\.(?:25[0-5]|2[0-4]\d|1?\d?\d)){3})(?:/(?:3[0-2]|[12]?\d))?\b`)
 func processData(data []byte, outDir, name string) error {
 	lines := strings.Split(string(data), "\n")
 
