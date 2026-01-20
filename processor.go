debfae6c3406457d7e021bfeeff628bd92283d25
a148ru <ant.band@mail.ru>
2026-01-20 13:48:54 +0300

    correct 1

--- processor.go
+++ processor.go
@@ -12,6 +12,7 @@ import (
 // \b(?:(?:25[0-5]|2[0-4]\d|1?\d?\d)(?:\.(?:25[0-5]|2[0-4]\d|1?\d?\d)){3})(?:/(?:3[0-2]|[12]?\d))?\b
 
 var netRegex = regexp.MustCompile(`\b(?:(?:25[0-5]|2[0-4]\d|1?\d?\d)(?:\.(?:25[0-5]|2[0-4]\d|1?\d?\d)){3})(?:/(?:3[0-2]|[12]?\d))?\b`)
+var netCIDRRegex = regexp.MustCompile(`\b\d{1,3}(\.\d{1,3}){3}/\d{1,2}\b`)
 func processData(data []byte, outDir, name string) error {
 	lines := strings.Split(string(data), "\n")
 
@@ -25,7 +26,11 @@ func processData(data []byte, outDir, name string) error {
 	for _, line := range lines {
 		if netRegex.MatchString(line) {
 			network := netRegex.FindString(line)
-			fmt.Fprintf(f, "route %s reject;\n", network)
+			if netCIDRRegex.MatchString(line){
+				fmt.Fprintf(f, "route %s reject;\n", network)
+			} else {
+				fmt.Fprintf(f, "route %s/32 reject;\n", network)
+			}
 		}
 	}
 	return nil
